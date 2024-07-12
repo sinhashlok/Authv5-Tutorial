f4d1681e89ab673e621d106cc32fa22b3acbdba0
sinhashlok <shlokjp@gmail.com>
2024-07-12 14:24:24 +0530

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/LoginButton";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <main className="h-full flex flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl text-white font-semibold drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth 🔐
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default page
+export default page;
